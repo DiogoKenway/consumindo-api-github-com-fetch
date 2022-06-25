7151e3719bde505626490a875e315fc61a39edbf
Diogo Alves <diogodidi2022@gmail.com>
2022-06-25 15:13:41 -0300

    refatorando

--- src/scripts/services/activity.js
+++ src/scripts/services/activity.js
@@ -8,26 +8,4 @@ async function getActivityUser(userName) {
 
 export { getActivityUser }
 
-const getEventType = data => data.filter(eventType => eventType.type == 'PushEvent')
-
-
-async function getUserCommits(user) {
-    getActivityUser(user).then(userData => {
-
-        const commitsMessages = getEventType(userData).map(data => data.payload.commits[0].message)
-        console.log(commitsMessages)
-    })
-
-}
-
-async function getRepositoriesName(user) {
-    getActivityUser(user).then(data => {
-        
-        const filterRepositoriesName = getEventType(data).map(repoName => repoName.repo.name)
-        console.log(filterRepositoriesName)
-    })
-}
-
-getRepositoriesName('diogokenway')
-getUserCommits('diogokenway')
 
